75743fc1c42b8c7fedf18af67f5164d1b4d2f53f
Dannyyqt <dannyyqthong@gmail.com>
2025-02-18 14:10:34 +0800

    data ingestion module completed

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    # train_data_path: str = os.path.join("artifacts", "train.csv")
+    # test_data_path: str = os.path.join("artifiacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+    base_data_path: str = os.path.join("data", "all_kindle_review.csv")
+
+
+class DataIngestion:
+    def __init__(self):
+        """
+        Initializes the DataIngestion instance with a default configuration.
+
+        Attributes:
+            ingestion_config (DataIngestionConfig): Configuration object containing file paths for train, test, and raw data.
+        """
+
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            df = pd.read_csv(self.ingestion_config.base_data_path)
+            logging.info("Read the dataset as dataframe")
+
+            # create artifact folder
+            directory = os.path.dirname(self.ingestion_config.raw_data_path)
+            os.makedirs(directory, exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info(f"Shape of raw data: {df.shape}")
+            logging.info("Data ingestion process completed successfully.")
+            return self.ingestion_config.base_data_path
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
